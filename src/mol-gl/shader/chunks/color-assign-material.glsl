402dd629721e0c0ddba24d27424f0f4793047d32
Alexander Rose <alex.rose@rcsb.org>
2018-06-01 14:51:34 -0700

    shader assignment fix

--- src/mol-gl/shader/chunks/color-assign-material.glsl
+++ src/mol-gl/shader/chunks/color-assign-material.glsl
@@ -1,5 +1,5 @@
 #if defined(dColorType_uniform)
-    vec4 material.rgb = uColor;
+    vec4 material = vec4(uColor, 1.0);
 #elif defined(dColorType_attribute) || defined(dColorType_instance) || defined(dColorType_element) || defined(dColorType_elementInstance) || defined(dColorType_objectPicking) || defined(dColorType_instancePicking) || defined(dColorType_elementPicking)
     vec4 material = vColor;
 #endif
