a879c05b1fb0b356a38fff65d54d69db44a245e3
Alexander Rose <alexander.rose@weirdbyte.de>
2018-06-11 07:17:28 +0200

    fix precision issues when rendering marker

--- src/mol-gl/shader/chunks/apply-marker-color.glsl
+++ src/mol-gl/shader/chunks/apply-marker-color.glsl
@@ -1,6 +1,6 @@
-float marker = floor(vMarker * 255.0);
-if (marker != 0.0) {
-    if (mod(marker, 2.0) == 0.0) {
+float marker = vMarker * 255.0;
+if (marker > 0.1) {
+    if (mod(marker, 2.0) < 0.1) {
         gl_FragColor.rgb = mix(uHighlightColor, gl_FragColor.rgb, 0.3);
     } else {
         gl_FragColor.rgb = mix(uSelectColor, gl_FragColor.rgb, 0.3);
