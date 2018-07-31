2d72a07444f0e670cc314aab083abd215e80cfa1
David Sehnal <david.sehnal@gmail.com>
2018-07-31 14:31:12 +0200

    updated to typescript 3.0

--- src/mol-util/read.ts
+++ src/mol-util/read.ts
@@ -12,7 +12,7 @@ export const readFileAs = (file: File, isBinary = false) => {
             reject(new DOMException('Error parsing file.'))
         }
         fileReader.onload = () => {
-            resolve(isBinary ? new Uint8Array(fileReader.result) : fileReader.result)
+            resolve(isBinary ? new Uint8Array(fileReader.result as ArrayBuffer) : fileReader.result as string)
         }
         if (isBinary) {
             fileReader.readAsArrayBuffer(file)
