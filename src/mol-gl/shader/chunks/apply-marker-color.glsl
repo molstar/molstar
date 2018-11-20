61d617c52cd964febdac2f8a5c76cbb6e4433c66
Alexander Rose <alex.rose@rcsb.org>
2018-11-19 17:04:39 -0800

    only mark elements with an alpha above the picking threshold

--- src/mol-gl/shader/chunks/apply-marker-color.glsl
+++ src/mol-gl/shader/chunks/apply-marker-color.glsl
@@ -1,8 +1,11 @@
-float marker = vMarker * 255.0;
-if (marker > 0.1) {
-    if (mod(marker, 2.0) > 0.1) {
-        gl_FragColor.rgb = mix(uHighlightColor, gl_FragColor.rgb, 0.3);
-    } else {
-        gl_FragColor.rgb = mix(uSelectColor, gl_FragColor.rgb, 0.3);
+// only mark elements with an alpha above the picking threshold
+if (uAlpha >= uPickingAlphaThreshold) {
+    float marker = vMarker * 255.0;
+    if (marker > 0.1) {
+        if (mod(marker, 2.0) > 0.1) {
+            gl_FragColor.rgb = mix(uHighlightColor, gl_FragColor.rgb, 0.3);
+        } else {
+            gl_FragColor.rgb = mix(uSelectColor, gl_FragColor.rgb, 0.3);
+        }
     }
 }
