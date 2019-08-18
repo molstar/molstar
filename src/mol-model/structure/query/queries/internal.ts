1c58bca454e8c1e580cde10069435d5209f00e22
Alexander Rose <alexander.rose@weirdbyte.de>
2019-08-18 12:58:01 -0700

    fix wrong props object in Structure creation

--- src/mol-model/structure/query/queries/internal.ts
+++ src/mol-model/structure/query/queries/internal.ts
@@ -41,7 +41,7 @@ export function atomicSequence(): StructureQuery {
 
             units.push(unit);
         }
-        return StructureSelection.Singletons(inputStructure, new Structure(units, inputStructure, ));
+        return StructureSelection.Singletons(inputStructure, new Structure(units, { parent: inputStructure }));
     };
 }
 
@@ -60,7 +60,7 @@ export function water(): StructureQuery {
             if (P.entity.type(l) !== 'water') continue;
             units.push(unit);
         }
-        return StructureSelection.Singletons(inputStructure, new Structure(units, inputStructure));
+        return StructureSelection.Singletons(inputStructure, new Structure(units, { parent: inputStructure }));
     };
 }
 
@@ -90,7 +90,7 @@ export function atomicHet(): StructureQuery {
 
             units.push(unit);
         }
-        return StructureSelection.Singletons(inputStructure, new Structure(units, inputStructure));
+        return StructureSelection.Singletons(inputStructure, new Structure(units, { parent: inputStructure }));
     };
 }
 
@@ -103,6 +103,6 @@ export function spheres(): StructureQuery {
             if (unit.kind !== Unit.Kind.Spheres) continue;
             units.push(unit);
         }
-        return StructureSelection.Singletons(inputStructure, new Structure(units, inputStructure));
+        return StructureSelection.Singletons(inputStructure, new Structure(units, { parent: inputStructure }));
     };
 }
