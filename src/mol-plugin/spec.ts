9ab400154466148736c29bc39e578772f705f563
Alexander Rose <alex.rose@rcsb.org>
2019-09-19 17:09:15 -0700

    get Behavior defaultParams from transformer params

--- src/mol-plugin/spec.ts
+++ src/mol-plugin/spec.ts
@@ -9,6 +9,7 @@ import { StateTransformer, StateAction } from '../mol-state';
 import { StateTransformParameters } from './ui/state/common';
 import { PluginLayoutStateProps } from './layout';
 import { PluginStateAnimation } from './state/animation/model';
+import { ParamDefinition as PD } from '../mol-util/param-definition';
 
 export { PluginSpec }
 
@@ -40,8 +41,11 @@ namespace PluginSpec {
         defaultParams?: any
     }
 
-    export function Behavior<T extends StateTransformer>(transformer: T, defaultParams?: StateTransformer.Params<T>): Behavior {
-        return { transformer, defaultParams };
+    export function Behavior<T extends StateTransformer>(transformer: T, defaultParams: Partial<StateTransformer.Params<T>> = {}): Behavior {
+        const params = transformer.definition.params
+            ? PD.getDefaultValues(transformer.definition.params(undefined, undefined))
+            : {}
+        return { transformer, defaultParams: { ...params, ...defaultParams } };
     }
 
     export interface LayoutControls {
