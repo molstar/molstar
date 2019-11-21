2757dfdb754d477e2f4c96cc7a40659beda8cdbd
Alexander Rose <alex.rose@rcsb.org>
2019-11-21 15:23:34 -0800

    add sleep util

---
+++ src/mol-util/sleep.ts
@@ -0,0 +1,9 @@
+/**
+ * Copyright (c) 2019 Mol* contributors, licensed under MIT, See LICENSE file for more info.
+ *
+ * @author Alexander Rose <alexander.rose@weirdbyte.de>
+ */
+
+export function sleep(milliseconds: number) {
+    return new Promise(resolve => setTimeout(resolve, milliseconds))
+}
