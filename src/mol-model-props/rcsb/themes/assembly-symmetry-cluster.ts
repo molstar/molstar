c5f336b0e40194c0cd1b295fbbab84962d773989
Alexander Rose <alex.rose@rcsb.org>
2020-02-25 14:53:37 -0800

    fix Assembly Symmetry Cluster coloring for deposited model

--- src/mol-model-props/rcsb/themes/assembly-symmetry-cluster.ts
+++ src/mol-model-props/rcsb/themes/assembly-symmetry-cluster.ts
@@ -59,11 +59,13 @@ export function AssemblySymmetryClusterColorTheme(ctx: ThemeDataContext, props: 
             for (let j = 0, jl = members.length; j < jl; ++j) {
                 const asymId = members[j]!.asym_id
                 const operList = [...members[j]!.pdbx_struct_oper_list_ids || []] as string[]
-                if (operList.length === 0) operList.push('1') // TODO hack assuming '1' is the id of the identity operator
                 clusterByMember.set(clusterMemberKey(asymId, operList), i)
+                if (operList.length === 0) {
+                    operList.push('1') // TODO hack assuming '1' is the id of the identity operator
+                    clusterByMember.set(clusterMemberKey(asymId, operList), i)
+                }
             }
         }
-
         const palette = getPalette(clusters.length, props)
         legend = palette.legend
 
