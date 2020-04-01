29d28bd3f4eca9b5c2a00ad8409eb60a4ce839b4
David Sehnal <david.sehnal@gmail.com>
2020-04-02 01:40:23 +0200

    UI tweaks

--- src/mol-plugin-ui/structure/measurements.tsx
+++ src/mol-plugin-ui/structure/measurements.tsx
@@ -177,7 +177,7 @@ export class MeasurementControls extends PurePluginUIComponent<{}, { isBusy: boo
     render() {
         return <>
             <div className='msp-flex-row'>
-                <ToggleButton icon='plus' label='Add' toggle={this.toggleAdd} isSelected={this.state.action === 'add'} disabled={this.state.isBusy} />
+                <ToggleButton icon='plus' label='Add' toggle={this.toggleAdd} isSelected={this.state.action === 'add'} disabled={this.state.isBusy} style={{ textAlign: 'left' }} />
                 <ToggleButton icon='cog' label='' title='Options' toggle={this.toggleOptions} isSelected={this.state.action === 'options'} disabled={this.state.isBusy} style={{ flex: '0 0 40px', padding: 0 }} />
             </div>
             {this.state.action === 'add' && this.add()}
