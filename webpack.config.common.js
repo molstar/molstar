53e028325fe4953ce7882db6baa981997c8e9ecd
Alexander Rose <alexander.rose@weirdbyte.de>
2020-04-14 17:09:00 -0700

    updated packages, removed font building code

--- webpack.config.common.js
+++ webpack.config.common.js
@@ -8,7 +8,7 @@ const sharedConfig = {
     module: {
         rules: [
             {
-                test: /\.(woff2?|ttf|otf|eot|svg|html|ico)$/,
+                test: /\.(html|ico)$/,
                 use: [{
                     loader: 'file-loader',
                     options: { name: '[name].[ext]' }
