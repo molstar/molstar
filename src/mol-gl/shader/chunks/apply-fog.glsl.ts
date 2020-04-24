be6fea39bf3849037002ba057007e6dfa45e4cc6
Alexander Rose <alexander.rose@weirdbyte.de>
2020-04-24 15:58:08 -0700

    fog and image shader tweaks

--- src/mol-gl/shader/chunks/apply-fog.glsl.ts
+++ src/mol-gl/shader/chunks/apply-fog.glsl.ts
@@ -1,8 +1,11 @@
 export default `
 float depth = length(vViewPosition);
 float fogFactor = smoothstep(uFogNear, uFogFar, depth);
+float fogAlpha = (1.0 - fogFactor) * gl_FragColor.a;
 if (uTransparentBackground == 0) {
     gl_FragColor.rgb = mix(gl_FragColor.rgb, uFogColor, fogFactor);
+    if (gl_FragColor.a < 1.0)
+        gl_FragColor.a = fogAlpha;
 } else {
     float fogAlpha = (1.0 - fogFactor) * gl_FragColor.a;
     gl_FragColor.a = fogAlpha;
