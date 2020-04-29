ea35e09c96404631b372ae77bfe2a3ff9bb29fd7
Alexander Rose <alexander.rose@weirdbyte.de>
2020-04-29 14:11:49 -0700

    focus repr carbon color tweaks

--- src/mol-plugin/behavior/dynamic/selection/structure-focus-representation.ts
+++ src/mol-plugin/behavior/dynamic/selection/structure-focus-representation.ts
@@ -26,11 +26,11 @@ const StructureFocusRepresentationParams = (plugin: PluginContext) => {
         expandRadius: PD.Numeric(5, { min: 1, max: 10, step: 1 }),
         targetParams: PD.Group(reprParams, {
             label: 'Target',
-            customDefault: createStructureRepresentationParams(plugin, void 0, { type: 'ball-and-stick', size: 'physical', typeParams: { sizeFactor: 0.17 } })
+            customDefault: createStructureRepresentationParams(plugin, void 0, { type: 'ball-and-stick', size: 'physical', typeParams: { sizeFactor: 0.17 }, colorParams: { carbon: Color.fromRgb(100, 100, 100) } })
         }),
         surroundingsParams: PD.Group(reprParams, {
             label: 'Surroundings',
-            customDefault: createStructureRepresentationParams(plugin, void 0, { type: 'ball-and-stick', color: 'element-symbol', size: 'physical', typeParams: { sizeFactor: 0.16 }, colorParams: { carbon: Color.fromRgb(200, 200, 200) } })
+            customDefault: createStructureRepresentationParams(plugin, void 0, { type: 'ball-and-stick', color: 'element-symbol', size: 'physical', typeParams: { sizeFactor: 0.16 }, colorParams: { carbon: Color.fromRgb(160, 160, 160) } })
         }),
         nciParams: PD.Group(reprParams, {
             label: 'Non-covalent Int.',
