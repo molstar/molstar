cbc941f19388c222f501ebf5520c5a36adcedd6f
Sebastian Bittrich <sebastian.bittrich@rcsb.org>
2022-05-06 09:08:22 -0700

    simplify array uniform check

--- src/mol-gl/webgl/uniform.ts
+++ src/mol-gl/webgl/uniform.ts
@@ -43,11 +43,7 @@ export function getUniformType(gl: GLRenderingContext, kind: UniformKind) {
 }
 
 export function isArrayUniform(kind: UniformKind) {
-    switch (kind) {
-        case 'b[]': case 'f[]': case 'i[]': case 'v2[]': case 'v3[]': case 'v4[]': case 'm3[]': case 'm4[]': return true;
-        case 'b': case 'f': case 'i': case 'v2': case 'v3': case 'v4': case 'm3': case 'm4': return false;
-        default: console.error(`unknown uniform kind '${kind}'`);
-    }
+    return kind.endsWith('[]');
 }
 
 export type UniformSetter = (gl: GLRenderingContext, location: number, value: any) => void
