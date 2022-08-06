e879479b3d8f82f57db5df98e40f8e2825d70854
yakomaxa <47655565+yakomaxa@users.noreply.github.com>
2022-08-07 07:36:39 +0900

    removed mock-extension for language selector and reverted src/mol-plugin-state/transforms/representation.ts to the original

--- src/apps/viewer/app.ts
+++ src/apps/viewer/app.ts
@@ -5,7 +5,6 @@
  * @author Alexander Rose <alexander.rose@weirdbyte.de>
  */
 
-import { ScriptSetting } from '../../extensions/language-select';
 import { ANVILMembraneOrientation } from '../../extensions/anvil/behavior';
 import { CellPack } from '../../extensions/cellpack';
 import { DnatcoConfalPyramids } from '../../extensions/dnatco';
@@ -68,7 +67,6 @@ const Extensions = {
     'geo-export': PluginSpec.Behavior(GeometryExport),
     'ma-quality-assessment': PluginSpec.Behavior(MAQualityAssessment),
     'zenodo-import': PluginSpec.Behavior(ZenodoImport),
-    'script-language': PluginSpec.Behavior(ScriptSetting),
 };
 
 const DefaultViewerOptions = {
