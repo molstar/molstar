de3f7e911e8726cf935eae56045870484c92dc14
Alexander Rose <alexander.rose@weirdbyte.de>
2022-11-25 12:13:23 -0800

    add support for volume segmentation cif

---
+++ src/mol-io/reader/cif/schema/segmentation.ts
@@ -0,0 +1,27 @@
+/**
+ * Copyright (c) 2022 mol* contributors, licensed under MIT, See LICENSE file for more info.
+ *
+ * @author Alexander Rose <alexander.rose@weirdbyte.de>
+ */
+
+import { Column, Database } from '../../../../mol-data/db';
+import { DensityServer_Data_Schema } from './density-server';
+
+import Schema = Column.Schema
+
+const int = Schema.int;
+const float = Schema.float;
+
+export const Segmentation_Data_Schema = {
+    volume_data_3d_info: DensityServer_Data_Schema.volume_data_3d_info,
+    segmentation_data_table: {
+        set_id: int,
+        segment_id: int,
+    },
+    segmentation_data_3d: {
+        values: float
+    }
+};
+
+export type Segmentation_Data_Schema = typeof Segmentation_Data_Schema;
+export interface Segmentation_Data_Database extends Database<Segmentation_Data_Schema> {}
