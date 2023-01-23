5838ba24724eb06f48a176be26dbd966d67b563b
Adam Midlik <midlik@gmail.com>
2023-01-23 17:53:22 +0000

    Updated docs in src/examples/image-renderer/index.ts

--- src/examples/image-renderer/index.ts
+++ src/examples/image-renderer/index.ts
@@ -4,7 +4,7 @@
  * @author Adam Midlik <midlik@gmail.com>
  *
  * Example command-line application generating images of PDB structures
- * Build: npm install --no-save gl  // `gl` not listed in dependencies for performance reasons
+ * Build: npm install --no-save gl jpeg-js pngjs  // these packages are not listed in dependencies for performance reasons
  *        npm run build
  * Run:   node lib/commonjs/examples/image-renderer 1cbs ../outputs_1cbs/
  */
