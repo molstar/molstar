95d3ab83de35476fa13f07a2c541b8bcd51c22c0
Russell Parker <russell@benchling.com>
2023-04-13 10:22:48 -0400

    Alter getFileInfo to avoid `instanceof WebAPIClass` and simple renames

--- src/extensions/volumes-and-segmentations/index.ts
+++ src/extensions/volumes-and-segmentations/index.ts
@@ -19,7 +19,7 @@ import { VolsegEntryFromRoot, VolsegGlobalStateFromRoot, VolsegStateFromEntry } 
 import { VolsegUI } from './ui';
 
 
-const DEBUGGING = window.location.hostname === 'localhost';
+const DEBUGGING = window?.location?.hostname === 'localhost';
 
 export const VolsegVolumeServerConfig = {
     // DefaultServer: new PluginConfigItem('volseg-volume-server', DEFAULT_VOLUME_SERVER_V2),
