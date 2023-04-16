5ac435c6d103b6171fbb32b23cea7d595be1d326
Alexander Rose <alexander.rose@weirdbyte.de>
2023-04-16 12:54:09 -0700

    only calculate instance-grid if there are instances

--- src/mol-geo/geometry/transform-data.ts
+++ src/mol-geo/geometry/transform-data.ts
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2018-2022 mol* contributors, licensed under MIT, See LICENSE file for more info.
+ * Copyright (c) 2018-2023 mol* contributors, licensed under MIT, See LICENSE file for more info.
  *
  * @author Alexander Rose <alexander.rose@weirdbyte.de>
  */
@@ -114,7 +114,7 @@ export function updateTransformData(transformData: TransformData, invariantBound
         aInstance[i] = i;
     }
 
-    if (invariantBoundingSphere) {
+    if (invariantBoundingSphere && instanceCount > 0) {
         const instanceGrid = calcInstanceGrid({
             instanceCount,
             instance: aInstance,
