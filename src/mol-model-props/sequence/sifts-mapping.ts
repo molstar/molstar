1ee04d52f58641f9463bcd2ef25c1ebae3fd0b30
dsehnal <david.sehnal@gmail.com>
2023-07-06 20:29:41 +0200

    update header

--- src/mol-model-props/sequence/sifts-mapping.ts
+++ src/mol-model-props/sequence/sifts-mapping.ts
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2021 mol* contributors, licensed under MIT, See LICENSE file for more info.
+ * Copyright (c) 2021-23 mol* contributors, licensed under MIT, See LICENSE file for more info.
  *
  * @author David Sehnal <david.sehnal@gmail.com>
  */
