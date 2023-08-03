835381a4556ee64b5745de8ea9fd1015bd15dd5d
dsehnal <david.sehnal@gmail.com>
2023-08-03 16:03:24 +0200

    header

--- src/mol-model-formats/structure/util.ts
+++ src/mol-model-formats/structure/util.ts
@@ -1,7 +1,8 @@
 /**
- * Copyright (c) 2019-2022 mol* contributors, licensed under MIT, See LICENSE file for more info.
+ * Copyright (c) 2019-2023 mol* contributors, licensed under MIT, See LICENSE file for more info.
  *
  * @author Alexander Rose <alexander.rose@weirdbyte.de>
+ * @author David Sehnal <david.sehnal@gmail.com>
  */
 
 import { TokenBuilder, Tokens } from '../../mol-io/reader/common/text/tokenizer';
