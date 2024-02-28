2685e7f46653eda49918eafe6eba7d0199508c8e
Aliaksei Chareshneu <chareshneu.tech@gmail.com>
2024-02-28 10:05:35 +0300

    update volume color and alpha

--- src/extensions/new-volumes-and-segmentations/cvsx-data.ts
+++ src/extensions/new-volumes-and-segmentations/cvsx-data.ts
@@ -18,6 +18,37 @@ import { AnnotationMetadata } from "./volseg-api/data";
 
 const DEFAULT_SEGMENT_COLOR = Color.fromNormalizedRgb(0.8, 0.8, 0.8);
 
+// export function VolsegControls({ plugin, data, setData }: { plugin: PluginContext, data: VolsegUIData, setData: (d: VolsegUIData) => void }) {
+//     const entryData = data.activeNode?.data;
+//     if (!entryData) {
+//         return <p>No data!</p>;
+//     }
+//     if (!data.globalState) {
+//         return <p>No global state!</p>;
+//     }
+
+//     const params = {
+//         /** Reference to the active VolsegEntry node */
+//         entry: PD.Select(data.activeNode!.data.ref, data.availableNodes.map(entry => [entry.data.ref, entry.data.entryId]))
+//     };
+//     const values: PD.Values<typeof params> = {
+//         entry: data.activeNode!.data.ref,
+//     };
+
+//     const globalState = useBehavior(data.globalState.currentState);
+
+//     return <>
+//         <ParameterControls params={params} values={values} onChangeValues={next => setData(VolsegUIData.changeActiveNode(data, next.entry))} />
+
+//         <TimeFrameSlider entryData={entryData} />
+//         <ExpandGroup header='Global options'>
+//             <WaitingParameterControls params={VolsegGlobalStateParams} values={globalState} onChangeValues={async next => await data.globalState?.updateState(plugin, next)} />
+//         </ExpandGroup>
+
+//         <VolsegEntryControls entryData={entryData} key={entryData.ref} />
+//     </>;
+// }
+
 export interface OutputByFormat {
     format: string,
     visuals: any[],
@@ -53,7 +84,7 @@ export async function updateVisualsBasedOnAnnotations(annotations: AnnotationMet
             for (const visual of visuals) {
                 const update = plugin.build().to(visual.cell.transform.parent);
                 for (const visual of visuals) {
-                    update.to(visual).update(StateTransforms.Representation.VolumeRepresentation3D, p => { p.type.params.alpha = 0.5; });
+                    update.to(visual).update(StateTransforms.Representation.VolumeRepresentation3D, p => { p.type.params.alpha = 0.2; p.colorTheme.params.value = Color.fromHexStyle('#000000')});
                     // TODO: update volume channel annotations if any similar to labels
                     // TODO: update isolevel?
                     // const isoLevelPromise = ExternalAPIs.tryGetIsovalue(this.entryData.metadata.raw.annotation?.entry_id.source_db_id ?? this.entryData.entryId);
@@ -111,10 +142,6 @@ export async function processCvsxFile(file: Asset.File, plugin: PluginContext, f
         return;
     }
 
-    // TODO: need to first parse annotations.json and pass this information
-    // to processCvsxFile for parsing segmentation
-
-
     // need to await so that the enclosing Task finishes after the update is done.
     const parsed = await provider.parse(plugin, data);
     if (visuals) {
@@ -128,91 +155,4 @@ export async function processCvsxFile(file: Asset.File, plugin: PluginContext, f
     }
 };
 
-// export interface CVSXData {
-//     /** MolViewSpec tree */
-//     // root: MVSTree,
-//     /** Associated metadata */
-//     // metadata: MVSMetadata,
-//     volume: 
-//     segmentation:
-// }
 
-// interface MVSMetadata {
-//     /** Version of the spec used to write this tree */
-//     version: string,
-//     /** Name of this view */
-//     title?: string,
-//     /** Detailed description of this view */
-//     description?: string,
-//     /** Format of the description */
-//     description_format?: 'markdown' | 'plaintext',
-//     /** Timestamp when this view was exported */
-//     timestamp: string,
-// }
-
-// export const CVSXData = {
-//     /** Currently supported major version of MolViewSpec format (e.g. 1 for version '1.0.8') */
-//     SupportedVersion: 1,
-
-//     /** Parse MVSJ (MolViewSpec-JSON) format to `MVSData`. Does not include any validation. */
-//     fromCVSX(data: {[k: string]: Uint8Array}): CVSXData {
-//         // const result: MVSData = JSON.parse(mvsjString);
-//         const result: CVSXData = Unzip(data)
-//         const major = majorVersion(result?.metadata?.version);
-//         if (major === undefined) {
-//             console.error('Loaded MVS does not contain valid version info.');
-//         } else if (major > (majorVersion(MVSData.SupportedVersion) ?? 0)) {
-//             console.warn(`Loaded MVS is of higher version (${result.metadata.version}) than currently supported version (${MVSData.SupportedVersion}). Some features may not work as expected.`);
-//         }
-//         return result;
-//     },
-
-//     /** Encode `MVSData` to MVSJ (MolViewSpec-JSON) string. Use `space` parameter to control formatting (as with `JSON.stringify`). */
-//     toMVSJ(mvsData: MVSData, space?: string | number): string {
-//         return JSON.stringify(mvsData, undefined, space);
-//     },
-
-//     /** Validate `MVSData`. Return `true` if OK; `false` if not OK.
-//      * If `options.noExtra` is true, presence of any extra node parameters is treated as an issue. */
-//     isValid(mvsData: MVSData, options: { noExtra?: boolean } = {}): boolean {
-//         return MVSData.validationIssues(mvsData, options) === undefined;
-//     },
-
-//     /** Validate `MVSData`. Return `undefined` if OK; list of issues if not OK.
-//      * If `options.noExtra` is true, presence of any extra node parameters is treated as an issue. */
-//     validationIssues(mvsData: MVSData, options: { noExtra?: boolean } = {}): string[] | undefined {
-//         const version = mvsData?.metadata?.version;
-//         if (typeof version !== 'string') return [`"version" in MVS must be a string, not ${typeof version}: ${version}`];
-//         if (mvsData.root === undefined) return [`"root" missing in MVS`];
-//         return treeValidationIssues(MVSTreeSchema, mvsData.root, options);
-//     },
-
-//     /** Return a human-friendly textual representation of `mvsData`. */
-//     toPrettyString(mvsData: MVSData): string {
-//         const title = mvsData.metadata.title !== undefined ? ` "${mvsData.metadata.title}"` : '';
-//         return `MolViewSpec tree${title} (version ${mvsData.metadata.version}, created ${mvsData.metadata.timestamp}):\n${treeToString(mvsData.root)}`;
-//     },
-
-//     /** Create a new MolViewSpec builder containing only a root node. Example of MVS builder usage:
-//      *
-//      * ```
-//      * const builder = MVSData.createBuilder();
-//      * builder.canvas({ background_color: 'white' });
-//      * const struct = builder.download({ url: 'https://www.ebi.ac.uk/pdbe/entry-files/download/1og2_updated.cif' }).parse({ format: 'mmcif' }).modelStructure();
-//      * struct.component().representation().color({ color: '#3050F8' });
-//      * console.log(MVSData.toPrettyString(builder.getState()));
-//      * ```
-//      */
-//     createBuilder(): Root {
-//         return createMVSBuilder();
-//     },
-// };
-
-
-// /** Get the major version from a semantic version string, e.g. '1.0.8' -> 1 */
-// function majorVersion(semanticVersion: string | number): number | undefined {
-//     if (typeof semanticVersion === 'string') return parseInt(semanticVersion.split('.')[0]);
-//     if (typeof semanticVersion === 'number') return Math.floor(semanticVersion);
-//     console.error(`Version should be a string, not ${typeof semanticVersion}: ${semanticVersion}`);
-//     return undefined;
-// }
