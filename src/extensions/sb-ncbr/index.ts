6acf847b89a62bddd07ef03b7daf698c4f05e7a4
Dušan Veľký <velky.dusko@gmail.com>
2024-04-15 17:43:59 +0200

    add providers to index

--- src/extensions/sb-ncbr/index.ts
+++ src/extensions/sb-ncbr/index.ts
@@ -1,3 +1,4 @@
 export { SbNcbrPartialCharges } from './partial-charges/behavior';
 export { SbNcbrPartialChargesPreset } from './partial-charges/preset';
 export { SbNcbrPartialChargesPropertyProvider } from './partial-charges/property';
+export {TunnelsDataProvider, TunnelDataProvider, TunnelShapeProvider, TunnelShapeSimpleProvider } from './tunnels/representation';
