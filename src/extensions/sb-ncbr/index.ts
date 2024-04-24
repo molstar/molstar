37a75db9e6c58ca352e7600d4e6bcdd363eba797
Dušan Veľký <velky.dusko@gmail.com>
2024-04-24 18:49:10 +0200

    updated tunnel transformers

--- src/extensions/sb-ncbr/index.ts
+++ src/extensions/sb-ncbr/index.ts
@@ -1,4 +1,4 @@
 export { SbNcbrPartialCharges } from './partial-charges/behavior';
 export { SbNcbrPartialChargesPreset } from './partial-charges/preset';
 export { SbNcbrPartialChargesPropertyProvider } from './partial-charges/property';
-export {TunnelsDataProvider, TunnelDataProvider, TunnelShapeProvider, TunnelShapeSimpleProvider } from './tunnels/representation';
+export { TunnelsDataTransformer, TunnelsToTunnelTransformer, TunnelDataTransformer, TunnelShapeProvider } from './tunnels/representation';
--- src/extensions/sb-ncbr/tunnels/representation.ts
+++ src/extensions/sb-ncbr/tunnels/representation.ts
@@ -3,151 +3,81 @@ import { StateTransformer } from '../../../mol-state';
 import { TunnelStateObject, Tunnel, TunnelShapeParams, TunnelsStateObject } from './props';
 import { ParamDefinition as PD } from '../../../mol-util/param-definition';
 import { Mesh } from '../../../mol-geo/geometry/mesh/mesh';
-import { WebGLContext } from '../../../mol-gl/webgl/context';
 import { Task } from '../../../mol-task';
-import { Color } from '../../../mol-util/color';
 import { createTunnelShape, createSpheresShape } from './algorithm';
 
 const Transform = StateTransformer.builderFactory('sb-ncbr-tunnels');
 
-export const TunnelsDataProvider = Transform({
+export const TunnelsDataTransformer = Transform({
     name: 'tunnels-from-data',
-    display: { name: 'Channels' },
-    from: PluginStateObject.Data.String,
-    to: TunnelStateObject,
-    params: {
-        kind: PD.MappedStatic('raw', {
-            raw: PD.EmptyGroup(),
-            trajectory: PD.Group({
-                index: PD.Numeric(0)
-            })
-        }),
-        channel_index: PD.Numeric(0),
-        channel: PD.Select('CSATunnels_MOLE', PD.arrayToOptions([
-            'CSATunnels_MOLE',
-            'CSATunnels_Caver',
-            'ReviewedChannels_MOLE',
-            'ReviewedChannels_Caver',
-            'CofactorTunnels_MOLE',
-            'CofactorTunnels_Caver',
-            'TransmembranePores_MOLE',
-            'TransmembranePores_Caver',
-            'ProcognateTunnels_MOLE',
-            'ProcognateTunnels_Caver',
-            'AlphaFillTunnels_MOLE',
-            'AlphaFillTunnels_Caver'
-        ]))
-    } })({
-    apply({ a, params, cache }) {
-        const data = JSON.parse(a.data);
-        (cache as any).Channels = data.Channels;
-        const channel = data.Channels[params.channel][params.channel_index];
-        if (!channel) {
-            return new TunnelStateObject({
-                tunnel: { data: [], type: 'Empty', id: '' } as Tunnel
-            }, { label: 'Channel settings' });
-        }
-        return new TunnelStateObject({
-            tunnel: { data: channel.Profile, type: channel.Type, id: channel.Id } as Tunnel
-        }, { label: 'Channel settings' });
-    },
-    update({ a, b, newParams, cache }) {
-        const channel = (cache as any).Channels[newParams.channel][newParams.channel_index];
-        if (!channel) {
-            b.data.tunnel.type = 'Empty';
-            b.data.tunnel.id = '';
-            b.data.tunnel.data = [];
-            return StateTransformer.UpdateResult.Updated;
-        }
-        b.data.tunnel.type = channel.Type;
-        b.data.tunnel.id = channel.Id;
-        b.data.tunnel.data = channel.Profile;
-        return StateTransformer.UpdateResult.Updated;
-    }
-});
-
-export const TunnelDataProvider = Transform({
-    name: 'tunnel-from-data',
-    display: { name: 'Channel' },
-    from: PluginStateObject.Data.String,
-    to: TunnelStateObject,
-    params: {
-        kind: PD.MappedStatic('raw', {
-            raw: PD.EmptyGroup(),
-            trajectory: PD.Group({
-                index: PD.Numeric(0)
-            })
-        })
-    } })({
-    apply({ a, params }) {
-        const data = JSON.parse(a.data);
-        return new TunnelStateObject({
-            tunnel: { data: data.Profile, type: data.Type, id: data.Id } as Tunnel
-        }, { label: 'Channel settings' });
-    }
-});
-
-export const TunnelShapeProvider = Transform({
-    name: 'tunnel-shape',
-    display: { name: 'Channel' },
-    from: TunnelStateObject,
-    to: PluginStateObject.Shape.Provider,
-    params: {
-        webgl: PD.Value<WebGLContext | null>(null),
-        colorTheme: PD.Color(Color(0xff0000)),
-        visual: PD.MappedStatic(
-            'mesh',
-            {
-                mesh: PD.Group({ resolution: PD.Numeric(2) }),
-                spheres: PD.Group({ resolution: PD.Numeric(2) })
-            }
-        ),
-    },
-})({
-    apply({ a, params }) {
-        return Task.create('Tunnel Shape Representation', async ctx => {
-            return new PluginStateObject.Shape.Provider({
-                label: 'Channel',
-                data: { tunnel: a.data.tunnel, params },
-                params: Mesh.Params,
-                geometryUtils: Mesh.Utils,
-                getShape: (_, data) => data.params.visual.name === 'mesh'
-                    ? createTunnelShape(data.tunnel, data.params.webgl, data.params.visual.params.resolution, data.params.colorTheme)
-                    : createSpheresShape(data.tunnel, data.params.webgl, data.params.visual.params.resolution, data.params.colorTheme)
-            }, { label: `${a.data.tunnel.type} ${a.data.tunnel.id}` });
-        });
-    },
-});
-
-export const TunnelShapeSimpleProvider = Transform({
-    name: 'tunnel-shape',
-    display: { name: 'Channel' },
+    display: { name: 'Tunnels' },
     from: PluginStateObject.Root,
-    to: PluginStateObject.Shape.Provider,
+    to: TunnelsStateObject,
     params: {
-        data: PD.Value<Tunnel>({ data: [], type: '', id: '' }),
-        webgl: PD.Value<WebGLContext | null>(null),
-        colorTheme: PD.Color(Color(0xff0000)),
-        visual: PD.MappedStatic(
-            'mesh',
-            {
-                mesh: PD.Group({ resolution: PD.Numeric(2) }),
-                spheres: PD.Group({ resolution: PD.Numeric(2) })
-            }
-        ),
+        data: PD.Value<Tunnel[]>([]),
     },
 })({
     apply({ params }) {
+        return new TunnelsStateObject({ tunnels: params.data });
+    },
+});
+
+export const TunnelsToTunnelTransformer = Transform({
+    name: 'tunnel-from-tunnels',
+    display: { name: 'Tunnel Selection' },
+    from: TunnelsStateObject,
+    to: TunnelStateObject,
+    params: a => {
+        return {
+            projectTunnel: PD.Numeric(0, { min: 0, max: a!.data.tunnels.length - 1, step: 1 })
+        };
+    }
+})({
+    apply({ a, params }) {
+        return new TunnelStateObject({ tunnel: a.data.tunnels[params.projectTunnel] });
+    }
+});
+
+export const TunnelDataTransformer = Transform({
+    name: 'tunnel-from-data',
+    display: { name: 'Tunnel Entry' },
+    from: PluginStateObject.Root,
+    to: TunnelStateObject,
+    params: {
+        data: PD.Value<Tunnel>(undefined as any, { isHidden: true })
+    },
+})({
+    apply({ params }) {
+        return new TunnelStateObject({ tunnel: params.data });
+    },
+});
+
+export const TunnelShapeProvider = Transform({
+    name: 'tunnel-shape-provider',
+    display: { name: 'Tunnel' },
+    from: TunnelStateObject,
+    to: PluginStateObject.Shape.Provider,
+    params: a => {
+        return {
+            ...TunnelShapeParams,
+            samplingRate: PD.Numeric(1, { min: 1, max: a!.data.tunnel.data.length - 1, step: 1 }),
+        };
+    },
+})({
+    apply({ a, params }) {
         return Task.create('Tunnel Shape Representation', async ctx => {
             return new PluginStateObject.Shape.Provider({
-                label: 'Channel',
-                data: { params },
+                label: 'Surface',
+                data: { params, data: a.data },
                 params: Mesh.Params,
                 geometryUtils: Mesh.Utils,
-                getShape: (_, data) => data.params.visual.name === 'mesh'
-                    ? createTunnelShape(data.data, data.colorTheme, data.params.visual.params.resolution, data.webgl)
-                    : createSpheresShape(data.data, data.colorTheme, data.params.visual.params.resolution, data.webgl)
-            }, { label: `${params.data.type} ${params.data.id}` });
+                getShape: (_, data, __, mesh) => {
+                    if (data.params.visual.name === 'mesh' && !data.params.showRadii) {
+                        return createTunnelShape(data.data.tunnel, data.params.colorTheme, data.params.visual.params.resolution, data.params.samplingRate, data.params.fillFactor, data.params.webgl, mesh);
+                    }
+                    return createSpheresShape(data.data.tunnel, data.params.colorTheme, data.params.visual.params.resolution, data.params.samplingRate, data.params.fillFactor, data.params.showRadii, mesh);
+                }
+            }, { label: a.data.tunnel.props.label ?? 'Tunnel', description: a.data.tunnel.props.description ?? '' });
         });
     },
 });
