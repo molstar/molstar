505aa688d2af2d753b175dba71bf8954eb4037af
Neli Fonseca <neli@ebi.ac.uk>
2024-05-31 13:18:14 +0100

    Update src/apps/viewer/app.ts
    Co-authored-by: David Sehnal <dsehnal@users.noreply.github.com>

--- src/apps/viewer/app.ts
+++ src/apps/viewer/app.ts
@@ -423,7 +423,7 @@ export class Viewer {
         });
     }
 
-    async loadFullResolutionMap(emdbId: string, iso: number) {
+    async loadFullResolutionEMDBMap(emdbId: string, options: { isoValue: Volume.IsoValue }) {
         const plugin = this.plugin;
         const numericId = parseInt(emdbId.toUpperCase().replace('EMD-', ''));
         const url = `https://ftp.ebi.ac.uk/pub/databases/emdb/structures/EMD-${numericId}/map/emd_${numericId}.map.gz`;
