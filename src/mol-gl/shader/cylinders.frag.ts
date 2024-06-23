d1c2c8e83799946489af7d9081ba13fc5b746a66
giagitom <giagitom@gmail.com>
2024-06-23 23:42:27 +0200

    Updated headers and changelog

--- src/mol-gl/shader/cylinders.frag.ts
+++ src/mol-gl/shader/cylinders.frag.ts
@@ -2,6 +2,7 @@
  * Copyright (c) 2020-2024 mol* contributors, licensed under MIT, See LICENSE file for more info.
  *
  * @author Alexander Rose <alexander.rose@weirdbyte.de>
+ * @author Gianluca Tomasello <giagitom@gmail.com>
  */
 
 export const cylinders_frag = `
