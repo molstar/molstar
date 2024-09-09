9525185e5ef230a659f4509a815306b6fb23aa9b
Aliaksei Chareshneu <chareshneu.tech@gmail.com>
2024-09-09 09:36:12 +0200

    remove unused code

--- src/mol-plugin-ui/controls/line-graph/point-component.tsx
+++ src/mol-plugin-ui/controls/line-graph/point-component.tsx
@@ -6,51 +6,6 @@ import { Color } from '../../../mol-util/color';
 import { ControlPointData } from './line-graph-component';
 import { forwardRef, useState } from 'react';
 
-// export class PointComponent extends React.Component<any, {show: boolean, id: UUID, index: number}> {
-//     constructor(props: any) {
-//         super(props);
-//         this.state = { show: false, id: props.idm, index: props.index };
-
-//         this.handleHover = this.handleHover.bind(this);
-//         this.handleHoverOff = this.handleHoverOff.bind(this);
-//     }
-
-//     private handleHover() {
-//         this.setState({ show: true });
-//         const point: ControlPointData = {
-//             x: this.props.nX,
-//             alpha: this.props.nY
-//         };
-//         this.props.onmouseover(point);
-//     }
-
-//     private handleHoverOff() {
-//         this.setState({ show: false });
-//         this.props.onmouseover(undefined);
-//     }
-
-//     public render() {
-//         const rgb = Color.toRgb(this.props.color);
-//         const fill = `rgb(${rgb})`;
-//         return ([
-//             <circle
-//                 r="10"
-//                 key={`${this.props.id}circle`}
-//                 id={`${this.props.id}`}
-//                 cx={this.props.x}
-//                 cy={this.props.y}
-//                 onClick={this.props.onclick}
-//                 // onDoubleClick={this.props.delete(this.props.id)}
-//                 onMouseEnter={this.handleHover}
-//                 onMouseLeave={this.handleHoverOff}
-//                 onMouseDown={this.props.onmousedown}
-//                 fill={fill}
-//             >
-//             </circle>
-//         ]);
-//     }
-// }
-
 interface PointComponentProps {
     index: any
     onmouseover: any
