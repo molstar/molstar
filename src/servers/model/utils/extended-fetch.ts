989837923aa849159b97e9b966f5b9e8c0d9d1ff
Adam Midlik <midlik@gmail.com>
2024-11-20 11:34:54 +0000

    ModelServer: support for GS

---
+++ src/servers/model/utils/extended-fetch.ts
@@ -0,0 +1,42 @@
+/**
+ * Copyright (c) 2024 mol* contributors, licensed under MIT, See LICENSE file for more info.
+ *
+ * @author Adam Midlik <midlik@gmail.com>
+ */
+
+import { Storage } from '@google-cloud/storage';
+import fetch, { Response } from 'node-fetch';
+
+
+export async function extendedFetch(url: string, init?: fetch.RequestInit): Promise<Response> {
+    if (url.startsWith('gs://')) return fetch_GS(url, init);
+    else return fetch(url, init);
+}
+
+async function fetch_GS(url: string, init?: fetch.RequestInit): Promise<Response> {
+    const fields = parseGsUri(url);
+    const data = await downloadGs(fields.bucket, fields.file);
+    return new Response(data, init);
+}
+
+function parseGsUri(url: string) {
+    if (!url.startsWith('gs://')) throw new Error(`Invalid GS URL (must start with 'gs://'): ${url}`);
+    const bucket_file = url.slice('gs://'.length);
+    const slashIndex = bucket_file.indexOf('/'); // thank you JS for not having a useful split function
+    if (slashIndex < 0) throw new Error(`Invalid GS URL (missing slash): ${url}`);
+    return {
+        bucket: bucket_file.slice(0, slashIndex),
+        file: bucket_file.slice(slashIndex + 1, undefined),
+    };
+}
+
+let _gsClient: Storage | undefined;
+function getGsClient() {
+    return _gsClient ??= new Storage();
+}
+
+async function downloadGs(bucketName: string, srcFileName: string) {
+    const response = await getGsClient().bucket(bucketName).file(srcFileName).download({}); // decompress:true?, end inclusive!
+    const buffer = response[0];
+    return buffer;
+}
--- src/servers/model/utils/fetch-retry.ts
+++ src/servers/model/utils/fetch-retry.ts
@@ -1,11 +1,13 @@
 /**
- * Copyright (c) 2018 mol* contributors, licensed under MIT, See LICENSE file for more info.
+ * Copyright (c) 2018-2024 mol* contributors, licensed under MIT, See LICENSE file for more info.
  *
  * @author David Sehnal <david.sehnal@gmail.com>
+ * @author Adam Midlik <midlik@gmail.com>
  */
 
-import fetch, { Response } from 'node-fetch';
+import { type Response } from 'node-fetch';
 import { retryIf } from '../../../mol-util/retry-if';
+import { extendedFetch } from './extended-fetch';
 
 const RETRIABLE_NETWORK_ERRORS = [
     'ECONNRESET', 'ENOTFOUND', 'ESOCKETTIMEDOUT', 'ETIMEDOUT',
@@ -20,7 +22,7 @@ export async function fetchRetry(url: string, timeout: number, retryCount: numbe
     const controller = new AbortController();
     const id = setTimeout(() => controller.abort(), timeout);
     const signal = controller.signal as any; // TODO: fix type
-    const result = await retryIf(() => fetch(url, { signal }), {
+    const result = await retryIf(() => extendedFetch(url, { signal }), {
         retryThenIf: r => r.status === 408 /** timeout */ || r.status === 429 /** too many requests */ || (r.status >= 500 && r.status < 600),
         // TODO test retryCatchIf
         retryCatchIf: e => isRetriableNetworkError(e),
