81af0de7f4ca1e2b7d05165a43badc2e3c419b2d
dsehnal <david.sehnal@gmail.com>
2024-12-08 20:36:57 +0100

    headers

--- src/mol-plugin-ui/controls/slider.tsx
+++ src/mol-plugin-ui/controls/slider.tsx
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2018-2019 mol* contributors, licensed under MIT, See LICENSE file for more info.
+ * Copyright (c) 2018-2024 mol* contributors, licensed under MIT, See LICENSE file for more info.
  *
  * @author David Sehnal <david.sehnal@gmail.com>
  * @author Alexander Rose <alexander.rose@weirdbyte.de>
