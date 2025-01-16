d50375617a87a00f8c0e6eda129894ba0e9ebbf0
Andy Turner <agdturner.gmail.com>
2025-01-16 21:19:46 +0000

    updated cleneing script to make more concise

--- scripts/pwa-clean.js
+++ scripts/pwa-clean.js
@@ -7,19 +7,16 @@
 const fs = require('fs');
 const path = require('path');
 
-// Function to delete manifest files
-function deleteManifestFiles(dir) {
+// General function to delete files based on prefix and suffix
+function deleteFiles(dir, prefix, suffix) {
     fs.readdir(dir, (err, files) => {
         if (err) {
             console.error('Error reading directory:', err);
             return;
         }
-
         files.forEach(file => {
             const filePath = path.join(dir, file);
-            if (file.startsWith('manifest-') && file.endsWith('.webmanifest') ||
-                file.startsWith('sw-') && file.endsWith('.js')) {
-                // Delete manifest files
+            if (file.startsWith(prefix) && file.endsWith(suffix)) {
                 fs.unlink(filePath, err => {
                     if (err) {
                         console.error('Error deleting file:', err);
@@ -28,8 +25,19 @@ function deleteManifestFiles(dir) {
                     console.log(`Deleted: ${filePath}`);
                 });
             }
-       });
+        });
     });
 }
 
+// Function to delete manifest files
+function deleteManifestFiles(dir) {
+    deleteFiles(dir, 'manifest-', '.webmanifest');
+}
+
+// Function to delete service worker files
+function deleteServiceWorkerFiles(dir) {
+    deleteFiles(dir, 'sw-', '.js');
+}
+
 deleteManifestFiles(path.join(__dirname, '..'));
+deleteServiceWorkerFiles(__dirname);
