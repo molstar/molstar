8c2d3a577a2c9a3aa63c488e507a63232b7315ee
Russ Taylor <russ@reliasolve.com>
2025-02-04 12:20:14 -0500

    Organizing and commenting list structures. Specifying types where known.

--- src/mol-io/reader/kin/schema.ts
+++ src/mol-io/reader/kin/schema.ts
@@ -19,37 +19,46 @@ export interface Kinemage {
   pointmasterDict: { [k: string]: any },
   dotLists: DotList[],
   vectorLists: VectorList[],
-  ballLists: any[],
+  ballLists: BallList[],
   ribbonLists: RibbonObject[]
 }
 
 export interface DotList {
-  name?: string,
-  masterArray: any[],
-  labelArray: any[],
-  positionArray: any[],
-  colorArray: any[]
+  name?: string,                  ///< Optional name of the whole List
+  masterArray: any[],             ///< Array of master names per List, not per element
+  labelArray: string[],           ///< Array of labels per List, not per element
+  positionArray: number[],        ///< Catenation of x, y, z for each element, 3x as many as elements
+  colorArray: number[]            ///< Catenation of r, g, b for each element, 3x as many as elements
 }
 
 export interface VectorList {
-  name?: string,
-  masterArray: any[],
-  label1Array: string[],
-  label2Array: string[],
-  position1Array: number[],
-  position2Array: number[],
-  color1Array: number[],
-  color2Array: number[],
-  width: number[]
+  name?: string,                  ///< Optional name of the whole List
+  masterArray: any[],             ///< Array of master names per List, not per element
+  label1Array: string[],          ///< Array of labels per List, not per element
+  label2Array: string[],          ///< Array of labels per List, not per element
+  position1Array: number[],       ///< Catenation of x, y, z for each element, 3x as many as elements
+  position2Array: number[],       ///< Catenation of x, y, z for each element, 3x as many as elements
+  color1Array: number[],          ///< Catenation of r, g, b for each element, 3x as many as elements
+  color2Array: number[],          ///< Catenation of r, g, b for each element, 3x as many as elements
+  width: number[]                 ///< A single width per element
+}
+
+export interface BallList {
+  name?: string,                  ///< Optional name of the whole List
+  masterArray: any[],             ///< Array of master names per List, not per element
+  labelArray: string[],           ///< Array of labels per List, not per element
+  positionArray: number[],        ///< Catenation of x, y, z for each element, 3x as many as elements
+  colorArray: number[],           ///< Catenation of r, g, b for each element, 3x as many as elements
+  radiusArray: number[]           ///< A single radius per element
 }
 
 export interface RibbonObject {
-  labelArray: string[],
-  positionArray: number[],
-  breakArray: boolean[],
-  colorArray: number[],
-  name?: string,
-  masterArray: any[]
+  name?: string,                  ///< Optional name of the whole Ribbon
+  masterArray: any[],             ///< Array of master names per Ribbon, not per element
+  labelArray: string[],           ///< Array of labels per Ribbon, not per element
+  positionArray: number[],        ///< Catenation of x, y, z for each element, 3x as many as elements
+  colorArray: number[],           ///< Catenation of r, g, b for each element, 3x as many as elements
+  breakArray: boolean[]           ///< A single boolean per element indicating if there is a break there
 }
 
 
