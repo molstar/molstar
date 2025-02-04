481b763049ca556ce36b8e3fbe3d2d30e84fd21d
Russ Taylor <russ@reliasolve.com>
2025-02-04 09:54:26 -0500

    Moving interface definitions to scheme.ts for Kin file reader.

--- src/mol-io/reader/kin/schema.ts
+++ src/mol-io/reader/kin/schema.ts
@@ -4,6 +4,55 @@
  * @author ReliaSolve <russ@reliasolve.com>
  */
 
+export interface Kinemage {
+  kinemage?: number,
+  onewidth?: any,
+  '1viewid'?: string,
+  pdbfile?: string,
+  text: string,
+  texts: string[],
+  captions: string[],
+  caption: string,
+  groupDict: { [k: string]: { [k: string]: boolean } },
+  subgroupDict: { [k: string]: any },
+  masterDict: { [k: string]: { indent: boolean, visible: boolean } },
+  pointmasterDict: { [k: string]: any },
+  dotLists: DotList[],
+  vectorLists: VectorList[],
+  ballLists: any[],
+  ribbonLists: RibbonObject[]
+}
+
+export interface DotList {
+  name?: string,
+  masterArray: any[],
+  labelArray: any[],
+  positionArray: any[],
+  colorArray: any[]
+}
+
+export interface VectorList {
+  name?: string,
+  masterArray: any[],
+  label1Array: string[],
+  label2Array: string[],
+  position1Array: number[],
+  position2Array: number[],
+  color1Array: number[],
+  color2Array: number[],
+  width: number[]
+}
+
+export interface RibbonObject {
+  labelArray: string[],
+  positionArray: number[],
+  breakArray: boolean[],
+  colorArray: number[],
+  name?: string,
+  masterArray: any[]
+}
+
+
 import { Column } from '../../../mol-data/db';
 
 // @todo Point to format description
