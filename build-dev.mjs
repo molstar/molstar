0216bfb7e39c2f56e057a95e9e21dfc52b8f804f
dsehnal <david.sehnal@gmail.com>
2025-03-02 11:25:38 +0100

    wip dev build

--- build-dev.mjs
+++ build-dev.mjs
@@ -2,7 +2,7 @@ import * as esbuild from 'esbuild';
 import * as fs from 'fs';
 import * as path from 'path';
 import { sassPlugin } from 'esbuild-sass-plugin';
-import packageJSON from './package.json' with { type: 'json'};
+import { Logger } from 'sass';
 
 function mkDir(dir) {
     if (!fs.existsSync(dir)) {
@@ -12,14 +12,14 @@ function mkDir(dir) {
 
 function fileLoaderPlugin(options) {
     mkDir(options.out, { recursive: true });
-    
+
     return {
         name: 'file-loader',
         setup(build) {
             build.onLoad({ filter: /\.jpg$/ }, async (args) => {
                 const name = path.basename(args.path);
                 mkDir(path.resolve(options.out, 'images'));
-                fs.copyFileSync(args.path, path.resolve(options.out, 'images', name));
+                await fs.promises.copyFile(args.path, path.resolve(options.out, 'images', name));
                 return {
                     contents: `images/${name}`,
                     loader: 'text',
@@ -27,28 +27,19 @@ function fileLoaderPlugin(options) {
             });
             build.onLoad({ filter: /\.(html|ico)$/ }, async (args) => {
                 const name = path.basename(args.path);
-                fs.copyFileSync(args.path, path.resolve(options.out, name));
+                await fs.promises.copyFile(args.path, path.resolve(options.out, name));
                 return {
                     contents: '',
                     loader: 'empty',
                 }
             });
+            // build.onResolve({ filter: /\.(html|ico)$/ }, async (args) => {
+            //     console.log(args);
+            //     return { watchFiles: [args.path] };
+            // });
         },
     }
 }
-
-const versionPlugin = {
-    name: 'version',
-    setup(build) {
-        build.onStart(() => {
-            fs.writeFileSync(
-                path.resolve('./lib/mol-plugin/version.js'),
-                `export var PLUGIN_VERSION = '${packageJSON.version}';\nexport var PLUGIN_VERSION_DATE = new Date(typeof __MOLSTAR_DEBUG_TIMESTAMP__ !== 'undefined' ? __MOLSTAR_DEBUG_TIMESTAMP__ : ${new Date().valueOf()});`);
-        });
-    }
-}
-
-const start = Date.now();
 
 const apps = ['viewer', 'docking-viewer', 'mesoscale-explorer'];
 const examples = ['proteopedia-wrapper', 'basic-wrapper', 'lighting', 'alpha-orbitals', 'alphafolddb-pae', 'mvs-kinase-story', 'ihm-restraints'];
@@ -63,27 +54,48 @@ async function build(name, kind) {
         entry = `./src/${kind}s/${name}/index.tsx`;
     }
 
-    await esbuild.build({
+    const ctx = await esbuild.context({
         entryPoints: [entry],
         tsconfig: './tsconfig.json',
         bundle: true,
-        // minify: true,
         globalName: 'molstar',
         outfile: kind === 'app'
             ? `./build/${name}/molstar.js`
             : `./build/examples/${name}/index.js`,
         plugins: [
-            versionPlugin,
             fileLoaderPlugin({ out: prefix }),
-            sassPlugin({ type: 'css' }),
+            sassPlugin({
+                type: 'css',
+                logger: {
+                    warn: (msg) => console.warn(msg),
+                    debug: () => { },
+                }
+            }),
         ],
         external: ['crypto', 'fs', 'path', 'stream'],
         loader: {
         },
     });
+
+    ctx.watch();
 }
 
-await Promise.all(apps.map(name => build(name, 'app')));
-await Promise.all(examples.map(name => build(name, 'example')));
+build('viewer', 'app');
 
-console.log(`Build time: ${Date.now() - start}ms`);
+for (const app of apps) {
+    // build(app, 'app');
+}
+
+const ctx = await esbuild.context({});
+ctx.serve({
+    servedir: './build',
+    port: 5888,
+});
+
+console.log('Serving on http://localhost:5888');
+
+
+// await Promise.all(apps.map(name => build(name, 'app')));
+// await Promise.all(examples.map(name => build(name, 'example')));
+
+// console.log(`Build time: ${Date.now() - start}ms`);
