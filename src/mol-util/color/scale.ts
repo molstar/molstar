d24ea13e8e7db2547ff0f6aa0cda45b32c4282f0
Lukas Polak <admin@lukaspolak.cz>
2025-03-06 15:45:11 +0100

    Implement createDiscrete in ColorScale

--- src/mol-util/color/scale.ts
+++ src/mol-util/color/scale.ts
@@ -1,7 +1,8 @@
 /**
- * Copyright (c) 2018-2019 mol* contributors, licensed under MIT, See LICENSE file for more info.
+ * Copyright (c) 2018-2025 mol* contributors, licensed under MIT, See LICENSE file for more info.
  *
  * @author Alexander Rose <alexander.rose@weirdbyte.de>
+ * @author Lukáš Polák <admin@lukaspolak.cz>
  */
 
 import { Color, ColorListEntry } from './color';
@@ -98,6 +99,84 @@ export namespace ColorScale {
             },
             setDomain,
             get legend() { return ScaleLegend(minLabel, maxLabel, colors); }
+        };
+    }
+
+    export function createDiscrete(props: ColorScaleProps): ColorScale {
+        const { domain, reverse, listOrName } = { ...DefaultColorScaleProps, ...props };
+        const list = typeof listOrName === 'string' ? getColorListFromName(listOrName).list : listOrName;
+
+        const colors = reverse ? list.slice().reverse() : list;
+
+        let diff = 0, min = 0, max = 0;
+        function setDomain(_min: number, _max: number) {
+            min = _min;
+            max = _max;
+            diff = (max - min) || 1;
+        }
+        setDomain(domain[0], domain[1]);
+
+        const minLabel = defaults(props.minLabel, min.toString());
+        const maxLabel = defaults(props.maxLabel, max.toString());
+
+        let color: (v: number) => Color;
+
+        const hasOffsets = colors.every(c => Array.isArray(c));
+        if (hasOffsets) {
+            const sorted = [...colors] as [Color, number][];
+            sorted.sort((a, b) => a[1] - b[1]);
+
+            const src = sorted.map(c => c[0]);
+            const off = SortedArray.ofSortedArray(sorted.map(c => c[1]));
+            const max = src.length - 1;
+
+            color = (value: number) => {
+                if (colors.length === 0) {
+                    return Color.fromRgb(0, 0, 0);
+                }
+
+                const t = clamp((value - min) / diff, 0, 1);
+                const i = SortedArray.findPredecessorIndex(off, t);
+
+                if (i === 0) {
+                    return src[min] as Color;
+                } else if (i > max) {
+                    return src[max] as Color;
+                }
+
+                return src[i] as Color;
+            };
+        } else {
+            color = (value: number) => {
+                if (colors.length === 0) {
+                    return Color.fromRgb(0, 0, 0);
+                }
+
+                const intervalSize = diff / colors.length;
+
+                if (value <= min) {
+                    return colors[0] as Color;
+                } else if (value >= max) {
+                    return colors[colors.length - 1] as Color;
+                }
+
+                const i = Math.min(colors.length - 1, Math.floor((value - min) / intervalSize));
+                const ifl = Math.floor(i);
+
+                return colors[ifl] as Color;
+            };
+        }
+
+        return {
+            color,
+            colorToArray: (value: number, array: NumberArray, offset: number) => {
+                Color.toArray(color(value), array, offset);
+            },
+            normalizedColorToArray: (value: number, array: NumberArray, offset: number) => {
+                Color.toArrayNormalized(color(value), array, offset);
+            },
+            setDomain,
+            get legend() { return ScaleLegend(minLabel, maxLabel, [Color(0xff0000), Color(0x00ff00)]); }
         };
     }
 }
