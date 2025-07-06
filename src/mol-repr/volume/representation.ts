ed5374fab947700babd0ac516826b589752fc3e5
Alexander Rose <alexander.rose@weirdbyte.de>
2025-07-06 10:10:02 -0700

    improve volume visual group count update

--- src/mol-repr/volume/representation.ts
+++ src/mol-repr/volume/representation.ts
@@ -186,7 +186,7 @@ export function VolumeVisual<G extends Geometry, P extends VolumeParams & Geomet
                 if (newGeometry) {
                     ValueCell.updateIfChanged(renderObject.values.drawCount, Geometry.getDrawCount(newGeometry));
                     ValueCell.updateIfChanged(renderObject.values.uVertexCount, Geometry.getVertexCount(newGeometry));
-                    ValueCell.updateIfChanged(renderObject.values.uGroupCount, Geometry.getGroupCount(newGeometry)); // TODO: use locationIt.groupCount instead when updated for volume visuals
+                    ValueCell.updateIfChanged(renderObject.values.uGroupCount, locationIt.groupCount);
                 } else {
                     throw new Error('expected geometry to be given');
                 }
