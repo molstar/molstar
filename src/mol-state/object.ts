49db2d251fea809242703efa966ba46bcd3a7a9c
dsehnal <david.sehnal@gmail.com>
2025-08-19 19:30:28 +0200

    headers

--- src/mol-state/object.ts
+++ src/mol-state/object.ts
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2018 mol* contributors, licensed under MIT, See LICENSE file for more info.
+ * Copyright (c) 2018-2025 mol* contributors, licensed under MIT, See LICENSE file for more info.
  *
  * @author David Sehnal <david.sehnal@gmail.com>
  */
