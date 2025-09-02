15510cc2be7a08f77b6ef24fcc5d7928839cc15f
dsehnal <david.sehnal@gmail.com>
2025-09-02 09:01:30 +0200

    tweak

--- src/mol-io/reader/ccp4/parser.ts
+++ src/mol-io/reader/ccp4/parser.ts
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2018 mol* contributors, licensed under MIT, See LICENSE file for more info.
+ * Copyright (c) 2018-2020 mol* contributors, licensed under MIT, See LICENSE file for more info.
  *
  * @author Alexander Rose <alexander.rose@weirdbyte.de>
  */
