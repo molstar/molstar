56225b337d1df7d6250b8623b8593b1f1751925a
Alexander Rose <alexander.rose@weirdbyte.de>
2025-10-11 16:19:51 -0700

    header

--- src/mol-model-formats/structure/pdb/conect.ts
+++ src/mol-model-formats/structure/pdb/conect.ts
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2021-2023 mol* contributors, licensed under MIT, See LICENSE file for more info.
+ * Copyright (c) 2021-2025 mol* contributors, licensed under MIT, See LICENSE file for more info.
  *
  * @author Alexander Rose <alexander.rose@weirdbyte.de>
  * @author Yakov Pechersky <ffxen158@gmail.com>
