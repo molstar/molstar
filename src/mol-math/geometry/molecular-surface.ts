2558d6fada09ff909e1e89e91873eeb6bfacc273
Alexander Rose <alexander.rose@weirdbyte.de>
2025-11-23 19:01:11 -0800

    remove dependency between mol-math and mol-geo

--- src/mol-math/geometry/molecular-surface.ts
+++ src/mol-math/geometry/molecular-surface.ts
@@ -11,7 +11,6 @@ import { ParamDefinition as PD } from '../../mol-util/param-definition';
 import { RuntimeContext } from '../../mol-task';
 import { OrderedSet } from '../../mol-data/int/ordered-set';
 import { fillGridDim, PositionData } from './common';
-import { BaseGeometry } from '../../mol-geo/geometry/base';
 import { Boundary } from './boundary';
 import { GridLookup3D } from './lookup3d/grid';
 import { Box3D } from './primitives/box3d';
@@ -50,8 +49,8 @@ function getAngleTables(probePositions: number): AnglesTables {
 
 export const MolecularSurfaceCalculationParams = {
     probeRadius: PD.Numeric(1.4, { min: 0, max: 10, step: 0.1 }, { description: 'Radius of the probe tracing the molecular surface.' }),
-    resolution: PD.Numeric(0.5, { min: 0.01, max: 20, step: 0.01 }, { description: 'Grid resolution/cell spacing.', ...BaseGeometry.CustomQualityParamInfo }),
-    probePositions: PD.Numeric(36, { min: 12, max: 90, step: 1 }, { description: 'Number of positions tested for probe target intersection.', ...BaseGeometry.CustomQualityParamInfo }),
+    resolution: PD.Numeric(0.5, { min: 0.01, max: 20, step: 0.01 }, { description: 'Grid resolution/cell spacing.' }),
+    probePositions: PD.Numeric(36, { min: 12, max: 90, step: 1 }, { description: 'Number of positions tested for probe target intersection.' }),
 };
 export const DefaultMolecularSurfaceCalculationProps = PD.getDefaultValues(MolecularSurfaceCalculationParams);
 export type MolecularSurfaceCalculationProps = typeof DefaultMolecularSurfaceCalculationProps
