71ca396f17cdd7291f87767fc21fecb509d0d348
Lukas Polak <admin@lukaspolak.cz>
2025-12-17 20:24:01 +0100

    Remove residue-name.ts contribution info

--- src/mol-theme/color/residue-name.ts
+++ src/mol-theme/color/residue-name.ts
@@ -1,8 +1,7 @@
 /**
- * Copyright (c) 2018-2025 mol* contributors, licensed under MIT, See LICENSE file for more info.
+ * Copyright (c) 2018-2022 mol* contributors, licensed under MIT, See LICENSE file for more info.
  *
  * @author Alexander Rose <alexander.rose@weirdbyte.de>
- * @author Lukáš Polák <admin@lukaspolak.cz>
  */
 
 import { Color, ColorMap } from '../../mol-util/color';
