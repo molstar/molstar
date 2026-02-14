8c6969206d49041127657b270700a2936777d259
giagitom <giagitom@gmail.com>
2026-02-15 00:59:48 +0100

    Update copyright years to 2026

--- src/mol-geo/geometry/text/font-atlas.ts
+++ src/mol-geo/geometry/text/font-atlas.ts
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2019-2022 mol* contributors, licensed under MIT, See LICENSE file for more info.
+ * Copyright (c) 2019-2026 mol* contributors, licensed under MIT, See LICENSE file for more info.
  *
  * @author Alexander Rose <alexander.rose@weirdbyte.de>
  * @author Adam Midlik <midlik@gmail.com>
--- src/mol-gl/shader/text.frag.ts
+++ src/mol-gl/shader/text.frag.ts
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2019-2024 mol* contributors, licensed under MIT, See LICENSE file for more info.
+ * Copyright (c) 2019-2026 mol* contributors, licensed under MIT, See LICENSE file for more info.
  *
  * @author Alexander Rose <alexander.rose@weirdbyte.de>
  * @author Gianluca Tomasello <giagitom@gmail.com>
