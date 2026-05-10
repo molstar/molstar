3e7614d75c2c79faa27a431829e5846b4245d411
Alexander Rose <alexander.rose@weirdbyte.de>
2026-05-09 17:37:47 -0700

    move spec

--- src/extensions/kinemage/_spec/kin.spec.ts
+++ src/extensions/kinemage/_spec/kin.spec.ts
@@ -4,7 +4,7 @@
  * @author ReliaSolve <russ@reliasolve.com>
  */
 
-import { parseKin } from './parser';
+import { parseKin } from '../reader/parser';
 
 const kinString = `@kinemage 1
 @caption probe.2.26.021123, run Tue Apr 23 14:49:17 2024
@@ -23,7 +23,7 @@ const kinString = `@kinemage 1
 {"}hotpink P  'O' 32.729,45.605,11.052 {"}hotpink   'O' 32.572,45.765,11.173
 `;
 
-/// @todo Replace with more complex kinemage
+// @todo Replace with more complex kinemage
 const kinComplexString = `@kinemage 1
 @caption probe.2.26.021123, run Tue Apr 23 14:49:17 2024
  command: C:\tmp\cctbx_phenix\build\probe\exe\probe.exe -kin -mc -het -once -wat2wat -onlybadout -stdbonds water all 1ssxFH.pdb
@@ -45,7 +45,7 @@ describe('kin reader', () => {
     it('basic', async () => {
         const parsed = await parseKin(kinString).run();
         if (parsed.isError) return;
-        if (parsed.result.length != 1) return;
+        if (parsed.result.length !== 1) return;
         const kinemage = parsed.result[0];
 
         const vectors = kinemage.vectorLists;
@@ -55,7 +55,7 @@ describe('kin reader', () => {
         expect(element.name).toEqual('x');
         expect(element.position1Array.length).toEqual(7);
 
-        /// @todo Add more tests
+        // @todo Add more tests
 
         expect.assertions(3);
     });
@@ -64,7 +64,7 @@ describe('kin reader', () => {
         const parsed = await parseKin(kinComplexString).run();
         if (parsed.isError) return;
 
-        /// @todo Add more complex tests
+        // @todo Add more complex tests
 
     });
 });
