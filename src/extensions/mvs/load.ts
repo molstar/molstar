69329db02eb3bdb3047f7da510409da5a01fc75e
Adam Midlik <midlik@gmail.com>
2026-08-25 14:26:10 +0100

    MVS: isHidden true for custom structure properties

--- src/extensions/mvs/load.ts
+++ src/extensions/mvs/load.ts
@@ -401,10 +401,7 @@ const MolstarLoadingActions: LoadingActions<MolstarTree, MolstarLoadingContext> 
                 [MVSAnnotationTooltipsProvider.descriptor.name]: { tooltips: annotationTooltips },
                 [CustomTooltipsProvider.descriptor.name]: { tooltips: inlineTooltips },
             },
-            autoAttach: [
-                MVSAnnotationTooltipsProvider.descriptor.name,
-                CustomTooltipsProvider.descriptor.name,
-            ],
+            // autoAttach not needed for these properties because they have isHidden:true (explicit autoAttach here would override default autoAttach of other properties!)
         }); // CustomStructureProperties must be applied even when `annotationTooltips` and `inlineTooltips` are empty, otherwise tooltips would persists across MVS snapshots
         const inlineLabels = collectInlineLabels(node, context);
         if (inlineLabels.length > 0) {
